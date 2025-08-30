ee4bfee67f6f9f474004fea256134c891c327451
gabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-08-30 11:14:39 -0300

    Add accuracy calculation and prediction tests for LinearSVC model

--- main.py
+++ main.py
@@ -7,4 +7,18 @@ modelo.fit(utils.dados, utils.classes)
 animal_misterioso = [[1, 0, 1], [0, 0, 0], [1, 1, 0]]
 resultado = modelo.predict(animal_misterioso)
 
-print(f"Resultado: {resultado}")
+misterio1 = [1, 1, 1]
+misterio2 = [1, 1, 0]
+misterio3 = [0, 1, 1]
+
+testes = [misterio1, misterio2, misterio3]
+
+previsoes = modelo.predict(testes)
+
+teste_classes = [0, 1, 1]
+
+corretos = (previsoes == teste_classes).sum()
+total = len(teste_classes)
+taxa_de_acertos = (corretos / total) * 100
+
+print(f"Acurácia: {taxa_de_acertos:.2f}%")
