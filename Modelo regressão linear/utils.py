fb76b85ab21275a25f47ab87071a4b758bd66ad8
gabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-09-03 07:18:12 -0300

    Refactor utils.py to enhance correlation heatmap visualization

--- Modelo regressão linear/utils.py
+++ Modelo regressão linear/utils.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 dados = pd.read_csv('Preços_de_casas.csv')
 
@@ -7,4 +10,22 @@ dados = dados.drop(columns='Id')
 # Correlação
 corr = dados.corr()
 
+# Quais fatores estão mais correlacionados?
 
+# Gerar uma máscara para o triângulo superior
+
+mascara = np.zeros_like(corr, dtype=bool)
+mascara[np.triu_indices_from(mascara)] = True
+
+# Configurar a figura do matplotlib
+f, ax = plt.subplots(figsize=(11, 9))
+
+# Gerar o mapa de calor (heatmap)
+
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+sns.heatmap(corr, mask=mascara, cmap=cmap, vmax=1, vmin=-1, center=0,
+            square=True, linewidths=.5, annot=True, cbar_kws={'shrink': .5})
+
+# Exibir o mapa de calor (heatmap)
+plt.show()
