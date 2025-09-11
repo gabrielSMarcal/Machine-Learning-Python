cf446abbdf6508eb89eb53c07ba4d8029fa096cd
gabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-09-11 16:51:25 -0300

    Enhance utils.py with additional text processing functionality and add main.py file

--- Modelo linguagem natural/utils.py
+++ Modelo linguagem natural/utils.py
@@ -1,7 +1,28 @@
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
 df = pd.read_csv('dataset_avaliacoes.csv')
 
 # print(df.value_counts('sentimento'))
-print('positiva \n')
-print(df.avaliacao[2])
+# print('positiva \n')
+# print(df.avaliacao[2])
+
+text = ['Comprei um produto ótimo', 'Comprei um produto ruim']
+
+vetorizar = CountVectorizer()
+bag_of_words = vetorizar.fit_transform(text)
+# print(bag_of_words)
+
+matriz_esparsa = pd.DataFrame.sparse.from_spmatrix(bag_of_words, columns=vetorizar.get_feature_names_out())
+# print(matriz_esparsa)
+
+# vetorizar = CountVectorizer(lowercase=False)
+# bag_of_words = vetorizar.fit_transform(df.avaliacao)
+# print(bag_of_words.shape)
+
+vetorizar = CountVectorizer(lowercase=False, max_features=50)
+bag_of_words = vetorizar.fit_transform(df.avaliacao)
+# print(bag_of_words.shape)
+
+matriz_esparsa_avaliacoes = pd.DataFrame.sparse.from_spmatrix(bag_of_words, columns=vetorizar.get_feature_names_out())
+# print(matriz_esparsa_avaliacoes)
