4fb1958d01129aca95c847c27cab73ca47615928
gabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-09-13 12:16:53 -0300

    Refactor comments in processar_avaliacao function for clarity and accuracy

--- Modelo linguagem natural/aplicacao.py
+++ Modelo linguagem natural/aplicacao.py
@@ -18,13 +18,13 @@ def processar_avaliacao(avaliacao):
     # Passo 2: Remoção de palavras irrelevantes
     frase_processada = [palavra for palavra in tokens if palavra.lower() not in palavras_irrelevantes]
     
-    # Passo 3: Stemming
+    # Passo 3: Remover pontuação
     frase_processada = [palavra for palavra in frase_processada if palavra.isalpha()]
     
     # Passo 4: Remover acentuação
     frase_processada = [unidecode.unidecode(palavra) for palavra in frase_processada]
     
-    # Passo 5: Aplicar Stemming
+    # Passo 5: Stemming
     frase_processada = [stemmer.stem(palavra) for palavra in frase_processada]
     
     return ' '.join(frase_processada)
