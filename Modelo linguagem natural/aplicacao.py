4a9b97b7f6595b28f4f787a163d7998477f5b8bf
gabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-09-13 12:11:39 -0300

    Add text processing functions for evaluation, including tokenization, stopword removal, stemming, and accent removal

--- Modelo linguagem natural/aplicacao.py
+++ Modelo linguagem natural/aplicacao.py
@@ -1,6 +1,31 @@
 import joblib
-import matplotlib.pyplot as plt
+import nltk
+from nltk import tokenize
+from unidecode import unidecode
 
 tfidf               = joblib.load('tfidf_vectorizer.pkl')
 regressao_logistica = joblib.load('modelo_regressao_logistica.pkl')
+
+palavras_irrelevantes = nltk.corpus.stopwords.words('portuguese')
+token_espaco = tokenize.WordPunctTokenizer()
+stemmer = nltk.RSLPStemmer()
+
+def processar_avaliacao(avaliacao):
+    
+    # Passo 1: Tokenização
+    tokens = token_espaco.tokenize(avaliacao)
+
+    # Passo 2: Remoção de palavras irrelevantes
+    frase_processada = [palavra for palavra in tokens if palavra.lower() not in palavras_irrelevantes]
+    
+    # Passo 3: Stemming
+    frase_processada = [palavra for palavra in frase_processada if palavra.isalpha()]
+    
+    # Passo 4: Remover acentuação
+    frase_processada = [unidecode.unidecode(palavra) for palavra in frase_processada]
+    
+    # Passo 5: Aplicar Stemming
+    frase_processada = [stemmer.stem(palavra) for palavra in frase_processada]
+    
+    return ' '.join(frase_processada)
 
